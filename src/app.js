a7a38aeaf493e658c1007b6730482e2b312fb05a
jishnu anil <jishnuanil255@gmail.com>
2025-02-07 17:44:22 +0530

    backend is working fix

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
 // Middleware
 app.use(express.json());
 app.use(cors());
